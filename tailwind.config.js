3b7262c0df0351e8f8f0336675319b948aee4e4d
AlexanderPopcov <rezzone16@gmail.com>
2025-11-21 20:08:28 +0300

    Update project

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,3 +20,14 @@ module.exports = {
   darkMode: 'class',
   plugins: [],
 }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
